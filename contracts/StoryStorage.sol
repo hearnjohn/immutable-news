6a08406fe4c962a3a7eba66aaedebe447fa1dac4
John Hearn <john@Johns-MacBook-Pro.local>
2021-08-04 20:21:34 +0900

    added a few stubs, more functionality

--- contracts/StoryStorage.sol
+++ contracts/StoryStorage.sol
@@ -3,18 +3,34 @@ pragma solidity ^0.8.3;
 contract StoryStorage {
 
     mapping(uint => mapping(address => int)) storyVotes;
-    mapping(uint => Story) stories;
+
+    // We're gonna map the day to a mapping of stories by their storyId
+    mapping(uint => mapping(uint => Story)) stories;
 
     struct Story {
-        uint id;
+        string url;
+        bytes32 uploader;
         int netScore;
     }
 
     uint lastStoryId = 0;
+    uint day = 1;
     
-    function createStory() internal {
+    function createStory(string memory _url, bytes32 _uploader) public {
         require(lastStoryId < 25);
-        lastStoryId++;
-        stories[lastStoryId] = Story(lastStoryId, 0);
+        stories[day][++lastStoryId] = Story(_url, _uploader, 0);
+    }
+
+    // Function to put the day's top stories in storage and start the next day's process, increment the day variable
+    function newDay() internal {
+        ++day;
+        lastStoryId = 0;
+    }
+
+    // Function to pay successful publishers, should only be called by resetStories() method
+    // TODO: figure out what the payout will be. Does contract owner take a cut?
+    // Also do we loop through the stories to pay everyone out? Very gas-intensive
+    function payout() internal {
+
     }
 }
