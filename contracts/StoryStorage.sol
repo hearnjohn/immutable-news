30d1b48c95ca7c11de556222a7e9c45c5a594925
Ujjwal-N <ujjwal.nadhani@gmail.com>
2021-08-05 11:40:18 -0700

    updated story struct

--- contracts/StoryStorage.sol
+++ contracts/StoryStorage.sol
@@ -2,13 +2,14 @@ pragma solidity ^0.8.3;
 
 contract StoryStorage {
 
-    mapping(uint => mapping(address => int)) storyVotes;
+    mapping(uint => mapping(Story => int)) storyVotes;
 
     // We're gonna map the day to a mapping of stories by their storyId
     mapping(uint => mapping(uint => Story)) stories;
 
     struct Story {
-        string url;
+        string title;
+        string body;
         bytes32 uploader;
         int netScore;
     }
@@ -16,9 +17,9 @@ contract StoryStorage {
     uint lastStoryId = 0;
     uint day = 1;
     
-    function createStory(string memory _url, bytes32 _uploader) public {
+    function createStory(string memory _title, string memory _body, bytes32 _uploader) public {
         require(lastStoryId < 25);
-        stories[day][++lastStoryId] = Story(_url, _uploader, 0);
+        stories[day][++lastStoryId] = Story(_title, _body, _uploader, 0);
     }
 
     // Function to put the day's top stories in storage and start the next day's process, increment the day variable
