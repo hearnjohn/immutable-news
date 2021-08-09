fee5809bd635c52331389dfc3fa6f3a7c7c17f82
John Hearn <john@Johns-MacBook-Pro.local>
2021-08-09 15:04:30 +0900

    made changes

--- contracts/StoryStorage.sol
+++ contracts/StoryStorage.sol
@@ -2,35 +2,35 @@ pragma solidity ^0.8.3;
 
 contract StoryStorage {
 
-    mapping(uint => mapping(address => int)) storyVotes;
-
-    // We're gonna map the day to a mapping of stories by their storyId
-    mapping(uint => mapping(uint => Story)) stories;
+    // mapping(uint => int[]) storyVotes; // Keep votes in an int array mapped by day
+    mapping(uint => Story[]) stories;
 
     struct Story {
-        string url;
+        uint id;
+        string title;
+        string body;
         bytes32 uploader;
         int netScore;
     }
-
-    uint lastStoryId = 0;
+    uint lastStoryId = 1;
     uint day = 1;
     
-    function createStory(string memory _url, bytes32 _uploader) public {
-        require(lastStoryId < 25);
-        stories[day][++lastStoryId] = Story(_url, _uploader, 0);
+    function createStory(string memory _title, string memory _body, bytes32 _uploader) public {
+        require(stories[day].length <= 25);
+        stories[day].push(Story(lastStoryId++, _title, _body, _uploader, 0));
     }
 
-    // Function to put the day's top stories in storage and start the next day's process, increment the day variable
-    function newDay() internal {
-        ++day;
+    // Function will return the list of stories for the day
+    // Off chain we will compute the ten best stories for the day and store them in a list
+    // That list will be the input to another function that pushes the stories to Filecoin
+    function newDay() internal returns (Story[] memory) {
         lastStoryId = 0;
+        return stories[day++];
     }
-
-    // Function to pay successful publishers, should only be called by resetStories() method
-    // TODO: figure out what the payout will be. Does contract owner take a cut?
-    // Also do we loop through the stories to pay everyone out? Very gas-intensive
-    function payout() internal {
-
+    
+    // This function will take in an input of the top 10 stories from the previous day and
+    // replace the <= 25 stories currently stored in the stories global variable
+    function pushStories(Story[] memory _stories) public {
+        stories[day - 1] = _stories;
     }
 }
--- contracts/UserStorage.sol
+++ contracts/UserStorage.sol
@@ -5,8 +5,10 @@ import "./StoryStorage.sol";
 contract UserStorage is StoryStorage {
 
     mapping (bytes32 => uint) usernames;
-    mapping (address => User) addresses;
-    
+    mapping (address => uint) addresses;
+    mapping (uint => User) ids;
+    mapping (uint => mapping(address => int[])) storyVotes;
+
     struct User {
         uint id;
         bytes32 username;
@@ -17,44 +19,54 @@ contract UserStorage is StoryStorage {
 
     // *** Need to agree on a timeframe for membership. Week? Month? ***
     modifier activeMembership() {
-        require(86400 - (block.timestamp - addresses[msg.sender].membershipStart) > 0);
+        require(86400 - (block.timestamp - ids[addresses[msg.sender]].membershipStart) > 0);
         _;
     }
 
     function createUser(bytes32 _username) public payable {
         require(usernames[_username] == 0);
-        require(addresses[msg.sender].id == 0);
+        require(addresses[msg.sender] == 0);
+        require(msg.value == 0.005 ether, "A 30-day membership costs 0.005 ether!");
         ++lastUserId;
         usernames[_username] = lastUserId;
-        addresses[msg.sender] = User(lastUserId, _username, block.timestamp);
+        addresses[msg.sender] = lastUserId;
+        ids[lastUserId] = User(lastUserId, _username, block.timestamp);
     }
 
-
     // Need to agree on upload fee, also is submission by link the best way to go about this?
-    function uploadStory(string memory _url) public payable activeMembership() {
-        createStory(_url, addresses[msg.sender].username);
+    function uploadStory(string memory _title, string memory _body) public payable activeMembership() {
+        require(msg.value == .01 ether, "Uploading a story costs 0.01 ether!");
+        createStory(_title, _body, ids[addresses[msg.sender]].username);
     }
     
     // Have the users cast their votes for the story here
     // We need to make sure that the web3 call keeps _vote from taking on a value other than 1 or -1
     function castVote(uint _storyId, int _vote) public activeMembership() {
-        require(storyVotes[_storyId][msg.sender] == 0);
+        require(storyVotes[day][msg.sender][_storyId] == 0);
 
         stories[day][_storyId].netScore = (_vote == 1) ? stories[day][_storyId].netScore + 1 :
                                                          stories[day][_storyId].netScore - 1;
-        storyVotes[_storyId][msg.sender] = _vote;
+        storyVotes[day][msg.sender][_storyId] = _vote;
     }
 
     function changeVote(uint _storyId, int _vote) public activeMembership() {
-        require(storyVotes[_storyId][msg.sender] != 0);
+        require(storyVotes[day][msg.sender][_storyId] != 0);
 
         // Need to revert the net score of the story by subtracting the value of the last vote
-        stories[day][_storyId].netScore = (storyVotes[_storyId][msg.sender] == 1) ?
+        stories[day][_storyId].netScore = (storyVotes[day][msg.sender][_storyId] == 1) ?
                                       stories[day][_storyId].netScore - 1 :
                                       stories[day][_storyId].netScore + 1;
         // Need to adjust it for new vote value
         stories[day][_storyId].netScore = (_vote == 1) ?
                                       stories[day][_storyId].netScore + 1 :
                                       stories[day][_storyId].netScore - 1;
+    }
+
+    // The user will be responsible for claiming the payout once their story has been scored
+    function payout(uint _day, uint _storyId) public payable {
+        require(ids[addresses[msg.sender]].username == stories[_day][_storyId].uploader);
+        if (stories[_day][_storyId].netScore > 0 && address(this).balance >= .015 ether) {
+            payable(msg.sender).transfer(.015 ether);
+        }
     }
 }
