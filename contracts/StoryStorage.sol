83cd02fae645686ac2962345b7a74163787250a4
John Hearn <john@Johns-MacBook-Pro.local>
2021-08-13 09:33:28 +0900

    worked out the kinks

--- contracts/StoryStorage.sol
+++ contracts/StoryStorage.sol
@@ -1,36 +1,45 @@
 pragma solidity ^0.8.3;
 
 contract StoryStorage {
-
-    // mapping(uint => int[]) storyVotes; // Keep votes in an int array mapped by day
-    mapping(uint => Story[]) stories;
+    
+    mapping(uint => mapping(uint => Story)) stories;
 
     struct Story {
         uint id;
         string title;
         string body;
-        bytes32 uploader;
+        string uploader;
         int netScore;
     }
-    uint lastStoryId = 1;
-    uint day = 1;
+    uint nextStoryId = 0;
+    uint day = 0;
     
-    function createStory(string memory _title, string memory _body, bytes32 _uploader) public {
-        require(stories[day].length <= 25);
-        stories[day].push(Story(lastStoryId++, _title, _body, _uploader, 0));
+    function createStory(string memory _title, string memory _body, string memory _uploader) public {
+        require(nextStoryId < 26);
+        stories[day][nextStoryId] = Story(nextStoryId, _title, _body, _uploader, 0);
+        ++nextStoryId;
     }
 
     // Function will return the list of stories for the day
     // Off chain we will compute the ten best stories for the day and store them in a list
     // That list will be the input to another function that pushes the stories to Filecoin
     function newDay() internal returns (Story[] memory) {
-        lastStoryId = 0;
-        return stories[day++];
+        nextStoryId = 0;
+        Story[] memory dayStories;
+        for (uint i = 0; i < 25; ++i) {
+            dayStories[i] = stories[day][i];
+        }
+        day++;
+        return dayStories;
     }
     
     // This function will take in an input of the top 10 stories from the previous day and
     // replace the <= 25 stories currently stored in the stories global variable
     function pushStories(Story[] memory _stories) public {
-        stories[day - 1] = _stories;
+        for (uint i = 0; i < 10; ++i) {
+            stories[day - 1][i] = _stories[i];
+        }
     }
+    
+    // Need story getter function
 }
--- contracts/UserStorage.sol
+++ contracts/UserStorage.sol
@@ -4,14 +4,14 @@ import "./StoryStorage.sol";
 
 contract UserStorage is StoryStorage {
 
-    mapping (bytes32 => uint) usernames;
+    mapping (string => uint) usernames;
     mapping (address => uint) addresses;
     mapping (uint => User) ids;
-    mapping (uint => mapping(address => int[])) storyVotes;
+    mapping (uint => mapping(address => mapping(uint => int))) storyVotes;
 
     struct User {
         uint id;
-        bytes32 username;
+        string username;
         uint membershipStart;
     }
 
@@ -19,11 +19,11 @@ contract UserStorage is StoryStorage {
 
     // *** Need to agree on a timeframe for membership. Week? Month? ***
     modifier activeMembership() {
-        require(86400 - (block.timestamp - ids[addresses[msg.sender]].membershipStart) > 0);
+        require(2592000 - (block.timestamp - ids[addresses[msg.sender]].membershipStart) > 0);
         _;
     }
 
-    function createUser(bytes32 _username) public payable {
+    function createUser(string memory _username) public payable {
         require(usernames[_username] == 0);
         require(addresses[msg.sender] == 0);
         require(msg.value == 0.005 ether, "A 30-day membership costs 0.005 ether!");
@@ -42,7 +42,7 @@ contract UserStorage is StoryStorage {
     // Have the users cast their votes for the story here
     // We need to make sure that the web3 call keeps _vote from taking on a value other than 1 or -1
     function castVote(uint _storyId, int _vote) public activeMembership() {
-        require(storyVotes[day][msg.sender][_storyId] == 0);
+        require(storyVotes[day][msg.sender][_storyId] == 0 && _storyId < nextStoryId);
 
         stories[day][_storyId].netScore = (_vote == 1) ? stories[day][_storyId].netScore + 1 :
                                                          stories[day][_storyId].netScore - 1;
@@ -63,9 +63,11 @@ contract UserStorage is StoryStorage {
     }
 
     // The user will be responsible for claiming the payout once their story has been scored
+    // Can't claim reward for story until the day is over
     function payout(uint _day, uint _storyId) public payable {
-        require(ids[addresses[msg.sender]].username == stories[_day][_storyId].uploader);
+        require(keccak256(bytes(ids[addresses[msg.sender]].username)) == keccak256(bytes(stories[_day][_storyId].uploader)));
         if (stories[_day][_storyId].netScore > 0 && address(this).balance >= .015 ether) {
+            stories[_day][_storyId].netScore = -1;
             payable(msg.sender).transfer(.015 ether);
         }
     }
